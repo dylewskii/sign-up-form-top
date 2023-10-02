b10078d6f7d67397b8a9bfb9e5d90f9c4a40706a
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-02 14:19:27 +0200

    add basic form validation

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const firstName = document.getElementsByClassName("first_name");
-const lastName = document.getElementsByClassName("last_name");
-const emailAddress = document.getElementsByClassName("email_address");
-const phoneNumber = document.getElementsByClassName("tel_number");
-const password = document.getElementsByClassName("password");
-const passwordConfirmation = document.getElementsByClassName("password_confirmation");
+form.addEventListener('submit', e => {
+    const passwordInput = document.getElementById('password');
+    const passwordConfirmationInput = document.getElementById('password_confirmation');
+    const validationMessage = document.getElementById('validation-message');
 
+    const password = passwordInput.value;
+    const passwordConfirmation = passwordConfirmationInput.value;
+    const hasCapitalLetter = /[A-Z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const isLengthValid = password.length >= 6;
 
-let validate = function () {
-    if (password.value === "" || password.value == null) {
-        alert("Password can't be empty!")
+    if (!hasCapitalLetter || !hasNumber || !isLengthValid) {
+        validationMessage.innerText = 'Password must contain a capital letter, a number, and be at least 6 characters long.';
+        e.preventDefault();
+    } else if (password !== passwordConfirmation) {
+        validationMessage.innerText = 'Passwords do not match.';
+        e.preventDefault();
+    } else {
+        // Only redirect when all conditions are met
+        window.location.href = 'http://127.0.0.1:5500/submitted.html';
     }
-}
+});
